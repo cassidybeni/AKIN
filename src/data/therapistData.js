5ed9aa048bdc3eba322287013066a1ea891ef579
cassidybeni <cassidybeni@pursuit.org>
2021-08-21 12:30:08 -0400

    Add therapistData

---
+++ src/data/therapistData.js
@@ -0,0 +1,30 @@
+const therapistdata = [
+  {
+    id: 1,
+    name: "Babita Spinelli",
+    location: [40.76541908162676, -73.98061610772974],
+    specialties:
+      "Couples counseling, Divorce counseling, Narcissistic abuse support, Relationships, Anxiety, EMDR therapy, Individual therapy, Pre-marital counseling, Small business coaching, Narcissist abuse therapy and coaching, Executive functioning & coaching",
+  },
+  {
+    id: 2,
+    name: "Brittany Lynch",
+    location: [40.74729268872588, -73.98312560220901],
+    specialties:
+      "Major life transitions, Identity issues, Relationship challenges, Depression, Anxiety, Trauma, Mindfulness",
+  },
+  {
+    id: 3,
+    name: "Pei-Han Cheng",
+    location: [40.751308606068704, -73.98171917587139],
+    specialties:
+      "Relationships, Career counseling, Eating disorders, Identity development, Life transitions, Cultural adjustment",
+  },
+  {
+    id: 4,
+    name: "Letizia Adorna",
+    location: [40.751308606068704, -73.98171917587139],
+    specialties:
+      "Anxiety, Depression, Eye Movement Desensitization and Reprocessing Therapy (EMDR), Trauma, Health professionals' mental healths, Dissociative disorders, Flash technique",
+  },
+];
