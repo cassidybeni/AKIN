2f98c21c4a83732172fedf80e7f31d13d26eceec
Candice Agard <candiceagard@Candices-MacBook-Pro.local>
2021-08-21 14:27:24 -0400

    links added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import NavBar from './Components/NavBar';
+import Therapists from './Pages/Therapists';
 
 function App() {
   return (
+    
     <div className="App">
-
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route path="/Therapists">
+            <Therapists />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 
 export default function NavBar () {
     return (
         <div>
-            
+            <Link to="/Home">Home</Link>
+            <Link to="/Therapists">Therapists</Link>
+            <Link to="/About">About</Link>
+            <Link to="/Contact">Contact</Link>
         </div>
     )
 }
+
