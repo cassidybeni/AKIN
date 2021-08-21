6e6ed103a4f3a63f9cfce49c360f548a0ea23f34
Grace Bong <gracebong.park@gmail.com>
2021-08-21 15:42:33 -0400

    image tag

--- src/Components/Therapist.js
+++ src/Components/Therapist.js
@@ -4,6 +4,7 @@ function Therapist({ therapist }) {
   return (
     <div>
       <h3>{therapist.name}</h3>
+      <img src={therapist.img} alt={therapist.name} />
       <h4>{therapist.title}</h4>
     </div>
   );
