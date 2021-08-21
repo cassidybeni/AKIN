7fe7f1786d1412745378d7542ba3bd0fc3281b44
cassidybeni <cassidybeni@pursuit.org>
2021-08-21 15:26:22 -0400

    Display therapist title

--- src/Components/Therapist.js
+++ src/Components/Therapist.js
@@ -4,7 +4,7 @@ function Therapist({therapist}) {
     return (
         <div>
             <h3>{therapist.name}</h3>
-            <h3>{console.log(therapist)}</h3>
+            <h4>{therapist.title}</h4>
         </div>
     )
 }
