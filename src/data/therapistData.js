0c1e8fcaee301e3eb4c835d0e0ad1e50844d0d45
Candice Agard <candiceagard@Candices-MacBook-Pro.local>
2021-08-21 15:37:29 -0400

    Therapists url

--- src/data/therapistData.js
+++ src/data/therapistData.js
@@ -2,6 +2,7 @@ const therapistData = [
   {
     id: 1,
     name: "Babita Spinelli",
+    img: "https://i.ibb.co/3mJ4ZKn/Babita-Spinelli.png",
     title: "Licensed Psychoanalyst, LP",
     location: [40.76541908162676, -73.98061610772974],
     specialties:
@@ -10,6 +11,7 @@ const therapistData = [
   {
     id: 2,
     name: "Brittany Lynch",
+    img: "https://i.ibb.co/zsqwgXc/Brittany-Lynch.png",
     title: "Licensed Clinical Social Worker, LCSW",
     location: [40.74729268872588, -73.98312560220901],
     specialties:
@@ -18,6 +20,7 @@ const therapistData = [
   {
     id: 3,
     name: "Pei-Han Cheng",
+    img: "https://i.ibb.co/ZLD4TxW/Pei-Han-Chang.png",
     title: "Psychologist, PhD",
     location: [40.751308606068704, -73.98171917587139],
     specialties:
@@ -26,11 +29,15 @@ const therapistData = [
   {
     id: 4,
     name: "Letizia Adorna",
+    img: "https://i.ibb.co/VQLCQVj/Letizia-Adorna.png",
     title: "Licensed Clinical Social Worker, LCSW",
     location: [40.751308606068704, -73.98171917587139],
     specialties:
       "Anxiety, Depression, Eye Movement Desensitization and Reprocessing Therapy (EMDR), Trauma, Health professionals' mental healths, Dissociative disorders, Flash technique",
   },
 ];
+
+
+
 
 export default therapistData;
