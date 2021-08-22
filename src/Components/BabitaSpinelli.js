f56f9b8e1cc94baaf975ab77f0f026b071bf9b46
Grace Bong <gracebong.park@gmail.com>
2021-08-22 14:48:19 -0400

    therapist detail page

---
+++ src/Components/BabitaSpinelli.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function BabitaSpinelli() {
+    return (
+        <div>
+            babita spinelli page
+        </div>
+    )
+}
+
+export default BabitaSpinelli
---
+++ src/Components/CarmenLo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CarmenLo() {
+    return (
+        <div>
+            carmen lo page
+        </div>
+    )
+}
+
+export default CarmenLo
