2696f5b18efc8a9526b2bf185d18e0a20dd33789
Candice Agard <candiceagard@Candices-MacBook-Pro.local>
2021-08-22 13:03:52 -0400

    directions and hignlight of marker

--- src/Components/TherapistList.js
+++ src/Components/TherapistList.js
@@ -7,7 +7,7 @@ class TherapistList extends React.Component {
 
 
   render () {
-     return <iframe src="https://www.atlistmaps.com/map/51cac0db-0e90-4407-ae55-66d0e2116f09?share=true" allow="geolocation" width="100%" height="900px" frameborder="0" scrolling="no" allowfullscreen></iframe>
+     return <iframe src="https://www.atlistmaps.com/map/51cac0db-0e90-4407-ae55-66d0e2116f09?share=true" allow="geolocation" width="100%" height="890px" frameborder="0" scrolling="no" allowfullscreen></iframe>
     
   }
 }
