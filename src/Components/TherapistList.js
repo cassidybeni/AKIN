9c778025b14d0242d2d77f3e021ab19816fffde1
Candice Agard <candiceagard@candices-mbp.myfiosgateway.com>
2021-08-22 07:06:38 -0400

    google maps geocode, autocomplete functionality implemented

--- src/Components/TherapistList.js
+++ src/Components/TherapistList.js
@@ -1,6 +1,10 @@
 import React from "react";
 import therapistData from "../data/therapistData";
 import Therapist from "./Therapist";
+import 'antd/dist/antd.css';
+import { Descriptions } from 'antd';
+import Geocode from "react-geocode";
+import AutoComplete from 'react-google-autocomplete';
 import {
   InfoWindow,
   withScriptjs,
@@ -8,6 +12,8 @@ import {
   GoogleMap,
   Marker,
 } from "react-google-maps";
+
+Geocode.setApiKey("AIzaSyBnoGPtQhDDR4JWf2DRgjVJFi-FQNrWt70")
 
 class TherapistList extends React.Component {
   state = {
@@ -18,19 +24,108 @@ class TherapistList extends React.Component {
     zoom: 15,
     height: 400,
     mapPosition: {
-      lat: 0,
-      lng: 0,
+      lat: 40.76541908162676,
+      lng: -73.98061610772974,
     },
     markerPosition: {
-      lat: 0,
-      lng: 0,
+      lat: 40.76541908162676,
+      lng: -73.98061610772974,
     },
+  };
+
+
+  getCity = ( addressArray ) => {
+    let city = '';
+
+    for (let i = 0; i < addressArray.length; i++) {
+      if (addressArray[i].types[0] && 'administrative_area_level_2' === addressArray[i].types[0]) {
+        city = addressArray[i].long_name
+        return city;
+    };
+  };
+};
+
+  getArea = ( addressArray ) => {
+    let area = '';
+    
+    for (let i = 0; i < addressArray.length; i++) {
+      if (addressArray[i].types[0]) {
+        for (let j = 0; j < addressArray.length; j++) {
+          if ('sublocality_level_1' === addressArray[i].types[j] || 'locality' === addressArray[i].types[j]) {
+            area = addressArray[i].long_name;
+            return area;
+          }
+        }
+      }
+    }
   }
+
+  getState = ( addressArray ) => {
+    let state = '';
+
+    for (let i = 0; i < addressArray.length; i++) {
+      for (let i = 0; i < addressArray.length; i++) {
+        if (addressArray[i].types[0] && 'administrative_area_level_1' === addressArray[i].types[0]) {
+          state = addressArray[i].long_name;
+          return state;
+      }
+    }
+  }
+}
 
   onMarkerDragEnd = (event) => {
     let newLat = event.latLng.lat();
+    let newLng = event.latLng.lng();
 
+    Geocode.fromLatLng(newLat, newLng)
+    .then(response => {
+      const address = response.results[0].formatted_address,
+            addressArray = response.results[0].address_components,
+            city = this.getCity(addressArray),
+            area = this.getArea(addressArray),
+            state = this.getState(addressArray) 
+
+            this.setState({
+              address: ( address ) ? address: '',
+              area: ( area ) ? area : '',
+              city: ( city ) ? city : '',
+              state : ( state ) ? state : '',
+              markerPosition :  {
+                lat: newLat,
+                lng: newLng
+              },
+             mapPosition: {
+              lat: newLat,
+              lng: newLng
+             }     
+        })
+    });
     
+  };
+
+  onPlaceSelected = (place) => {
+    const address = place.formatted_address,
+      addressArray = place.address_components,
+      city = this.getCity(addressArray),
+      area = this.getArea(addressArray),
+      state = this.getState(addressArray), 
+      newLat = place.geometry.location.lat(),
+      newLng = place.geometry.location.lng()
+
+      this.setState({
+        address: ( address ) ? address: '',
+        area: ( area ) ? area : '',
+        city: ( city ) ? city : '',
+        state: ( state ) ? state : '',
+        markerPosition :  {
+          lat: newLat,
+          lng: newLng
+        },
+       mapPosition: {
+        lat: newLat,
+        lng: newLng
+       },
+    }) 
   }
 
 
@@ -38,22 +133,37 @@ class TherapistList extends React.Component {
 
     const MapWithAMarker = withScriptjs(withGoogleMap(props =>
       <GoogleMap
-        defaultZoom={8}
-        defaultCenter={{ lat: -34.397, lng: 150.644 }}
+        defaultZoom={12}
+        defaultCenter={{ lat: this.state.mapPosition.lat, lng: this.state.mapPosition.lng }}
       >
-        <Marker
+
+      <Marker
           draggable={true}
           onDragEnd={this.onMarkerDragEnd}
-          position={{ lat: -34.397, lng: 150.644 }}
-          InfoWindow
-        />
+          position={{ lat: this.state.markerPosition.lat, lng: this.state.markerPosition.lng }}
+          
+      />
+
+      <AutoComplete style={{ width: "100%", height: '40px', paddingLeft: '16px', marginTop: '2px,', marginBottom: '2rem'}}
+        types={['(regions)']}
+        onPlaceSelected={this.onPlaceSelected}
+      />
+
       </GoogleMap>
     ));
 
 
 
     return (
-      <div>
+      <div style={{ padding: '1rem', margin: '0 auto', maxWidth: 1000 }}>
+        <h1>Therapists</h1>
+          <Descriptions  bordered>
+              <Descriptions.Item label="City">{this.state.city}</Descriptions.Item>
+              <Descriptions.Item label="Area">{this.state.area}</Descriptions.Item>
+              <Descriptions.Item label="State">{this.state.state}</Descriptions.Item>
+              <Descriptions.Item label="Address">{this.state.address}</Descriptions.Item>
+              
+            </Descriptions>,
 
           <MapWithAMarker
             googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyBnoGPtQhDDR4JWf2DRgjVJFi-FQNrWt70&v=3.exp&libraries=geometry,drawing,places"
